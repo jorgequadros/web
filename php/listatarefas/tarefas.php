ed2dcfda3199cae8b389d56d03d9b08f05031655
jorgequadros <jorge.quadros77@outlook.com>
2023-11-12 14:19:20 -0300

    Atualização
    concluido

--- php/listatarefas/tarefas.php
+++ php/listatarefas/tarefas.php
@@ -50,21 +50,56 @@ if (isset($tarefa['prioridade'])){
 };*/
 session_start();
 
-if(array_key_exists('nome', $_GET)){
-    $_SESSION['lista_tarefas'][] = $_GET['nome'];
+if(array_key_exists('nome', $_GET) && $_GET['nome']!=''){
+    $tarefa=[];
+
+    $tarefa['nome']=$_GET['nome'];
+
+    if(array_key_exists('descricao',$_GET)){
+        $tarefa['descricao']=$_GET['descricao'];
+    }else{
+        $tarefa['descricao']='';
+    }
+
+    if(array_key_exists('prazo',$_GET)){
+        $tarefa['prazo']=$_GET['prazo'];
+    }else{
+        $tarefa['prazo']='';
+    }
+
+    $tarefa['prioridade']=$_GET['prioridade'];
+
+    if(array_key_exists('concluida',$_GET)){
+        $tarefa['concluida']=$_GET['concluida'];
+    }else{
+        $tarefa['concluida']='';
+    }
+
+    $_SESSION['lista_tarefas'][] = $tarefa;
+
+    $lista_tarefas=[];
+    $lista_tarefas=$_SESSION['lista_tarefas'];
 }
 
-$lista_tarefas=[];
 
-if(array_key_exists('lista_tarefas', $_SESSION)){
-    $lista_tarefas= $_SESSION['lista_tarefas'];
-}
 ?>
 
 <table>
-    <tr><th>Tarefas</th></tr>
+    <tr>
+        <th>Tarefas</th>
+        <th>Descrição</th>
+        <th>Prazo</th>
+        <th>Prioridade</th>
+        <th>Concluída</th>
+    </tr>
     <?php foreach ($lista_tarefas as $tarefa) :?>
-    <tr><td><?php echo $tarefa;?></td></tr>
+    <tr>
+        <td><?php echo $tarefa['nome'];?></td>
+        <td><?php echo $tarefa['descricao'];?></td>
+        <td><?php echo $tarefa['prazo'];?></td>
+        <td><?php echo $tarefa['prioridade'];?></td>
+        <td><?php echo $tarefa['concluida'];?></td>
+    </tr>
     <?php endforeach; ?>
 </table>
 
